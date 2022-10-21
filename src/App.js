de037bde2d97b9b063736c18b328c847cac229e8
Anantha M <anantha.m@mphasis.com>
2022-10-21 23:09:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import GitHubRepos from './component/js/container/GitHubRepos';
+import GitHubUsers from './component/js/container/GitHubUsers';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path='/' element={<GitHubUsers />} />
+      <Route path='/repo/:userId' element={<GitHubRepos />} />
+    </Routes>
   );
-}
+};
 
 export default App;
